820749eb23712b97cf24eb1f11ddcbbe46f12911
Keith Thornhill <keith.thornhill@gmail.com>
2018-04-30 19:09:06 -0700

    add more stats

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # graftorio
+
 visualize metrics from your factorio game in grafana
+
+## Installation
+
+- install the mod via the game client
+- install docker
+- download this repo
+- if not using windows, edit `docker-compose.yml` and uncomment the correct path to your factorio install
+- using a terminal, run `docker-compose up`
+- load `localhost:3000` in a browser
+- login with admin:admin and create a prometheus data source using `http://exporter:9090` as address
+- enable mod and load up your game
+- use grafana to define dashboard
--- control.lua
+++ control.lua
@@ -1,19 +1,22 @@
 prometheus = require("prometheus/prometheus")
 
-gauge_item_production_input = prometheus.gauge("item_production_input", "items produced", {"force", "name"})
-gauge_item_production_output = prometheus.gauge("item_production_output", "items consumed", {"force", "name"})
-gauge_fluid_production_input = prometheus.gauge("fluid_production_input", "fluids produced", {"force", "name"})
-gauge_fluid_production_output = prometheus.gauge("fluid_production_output", "fluids consumed", {"force", "name"})
+gauge_item_production_input = prometheus.gauge("factorio_item_production_input", "items produced", {"force", "name"})
+gauge_item_production_output = prometheus.gauge("factorio_item_production_output", "items consumed", {"force", "name"})
+gauge_fluid_production_input = prometheus.gauge("factorio_fluid_production_input", "fluids produced", {"force", "name"})
+gauge_fluid_production_output = prometheus.gauge("factorio_fluid_production_output", "fluids consumed", {"force", "name"})
+gauge_kill_count_input = prometheus.gauge("factorio_kill_count_input", "kills", {"force", "name"})
+gauge_kill_count_output = prometheus.gauge("factorio_kill_count_output", "losses", {"force", "name"})
+gauge_entity_build_count_input = prometheus.gauge("factorio_entity_build_count_input", "entities placed", {"force", "name"})
+gauge_entity_build_count_output = prometheus.gauge("factorio_entity_build_count_output", "entities removed", {"force", "name"})
 
 script.on_event(defines.events.on_tick, function(event)
   if event.tick % 600 == 0 then
     for _, player in pairs(game.players) do
-
       stats = {
         {player.force.item_production_statistics, gauge_item_production_input, gauge_item_production_output},
         {player.force.fluid_production_statistics, gauge_fluid_production_input, gauge_fluid_production_output},
-        -- player.force.kill_count_statistics,
-        -- player.force.entity_build_count_statistics,
+        {player.force.kill_count_statistics, gauge_kill_count_input, gauge_kill_count_output},
+        {player.force.entity_build_count_statistics, gauge_entity_build_count_input, gauge_entity_build_count_output},
       }
 
       for _, stat in pairs(stats) do
