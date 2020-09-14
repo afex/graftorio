0c1a74ba2fe0b3b032ca624afb812b590dd3f4a2
tim vroom <tim@timvroom.nl>
2020-09-14 22:40:14 +0200

    Forgot control.lua

--- control.lua
+++ control.lua
@@ -1,15 +1,19 @@
 prometheus = require("prometheus/prometheus")
 require("utility")
 local handler = require("event_handler")
-local translate = require("scripts/translation")
-local await_export = false
-
 gauges = {}
 histograms = {}
 
-function doExport()
-  game.write_file("graftorio/game.prom", prometheus.collect(), false)
-end
+-- All the events are split into the 10 second export time (600 ticks)
+--
+-- export: 0 - 10
+-- force_stats: 60, 80
+-- trains: 120
+-- power: 240
+-- plugins: 300
+-- statics: 520
+--
+-- The first few exports are slow since they require the translations
 
 handler.add_lib(require("scripts/statics"))
 handler.add_lib(require("scripts/force_stats"))
@@ -17,28 +21,7 @@ handler.add_lib(require("scripts/trains"))
 handler.add_lib(require("scripts/power"))
 handler.add_lib(require("scripts/plugins"))
 handler.add_lib(require("scripts/remote"))
-handler.add_lib(translate)
+handler.add_lib(require("scripts/translation"))
 
 -- Keep as last to export it all
-handler.add_lib(
-  {
-    ["on_nth_tick"] = {
-      [600] = function(event)
-        if translate.in_progress() then
-          await_export = true
-        else
-          doExport()
-          await_export = false
-        end
-      end
-    },
-    events = {
-      [defines.events.on_tick] = function(event)
-        if event.tick % 30 == 0 and await_export and not translate.in_progress() then
-          doExport()
-          await_export = false
-        end
-      end
-    }
-  }
-)
+handler.add_lib(require("scripts/export"))
